4c668ec98dd432e5198cdf12f37d1b8057cc0f20
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-06-24 06:34:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,20 +19,6 @@ channel.send({embed : embed});
 });
 		
 
-    client.on('guildMemberRemove', member => {
-        var embed = new Discord.RichEmbed()
-        .setAuthor(member.user.username, member.user.avatarURL)
-        .setThumbnail(member.user.avatarURL)
-        .setTitle(`لقد خرج صديق :( :raised_hand::skin-tone-1: :pensive:`)
-        .setDescription(`الله معك بالتوفيق :raised_hand::skin-tone-1: :pensive: `)
-        .addField(':bust_in_silhouette:   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
-        .setColor('RANDOM')
-        .setFooter(`====شكرا للاستمتاع====`, ' https://cdn.discordapp.com/attachments/397818254439219217/399292026782351381/shy.png')
-var channel =member.guild.channels.find('name', '🙌『الـترحيب』-•')
-if (!channel) return;
-channel.send({embed : embed});
-	
-	
 	
 	
 	
