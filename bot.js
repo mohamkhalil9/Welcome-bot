9614b0c6f95e41d4aaa251ebec10a95e78cb6308
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-06-24 05:34:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,9 +9,9 @@ client.on('guildMemberAdd', member => {
     var embed = new Discord.RichEmbed()
     .setThumbnail(member.user.avatarURL)
   .addField("***شكرا الانضمامك الينا***" ,member.user.username )
-    .setDescription('***بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا***')
+    .setDescription('**بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا**')
     .setColor('RANDOM')
-    .setImage('http://www.imgion.com/images/01/Welcome-buddy.jpg')
+    .setImage('https://cdn.discordapp.com/attachments/592539684664770561/592542955554865164/welcome_4.png')
 var channel =member.guild.channels.find('name', '🙌『الـترحيب』-•')
 if (!channel) return;
 channel.send({embed : embed});
