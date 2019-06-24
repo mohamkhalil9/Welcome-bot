e85b0b705a2d9eeafc36973d0409cbe4c3eac8cf
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-06-24 06:20:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('guildMemberAdd', member => {
     var embed = new Discord.RichEmbed()
     .setThumbnail(member.user.avatarURL)
   .addField("***شكرا الانضمامك الينا***" ,member.user.username )
-    .setDescription('**__نتمنى لك قضآء أجمل اللحظات ولآوقات معنا__**')
+    .setDescription('**__نتمنى لك قضآء أجمل اللحظات والأوقات معنا__**')
     .setColor('RANDOM')
     .setImage('https://cdn.discordapp.com/attachments/590669646806646806/592554419628015616/Untitled-1.png')
 var channel =member.guild.channels.find('name', '🙌『الـترحيب』-•')
